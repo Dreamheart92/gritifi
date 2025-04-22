7d037a386866b3dde62dc10270fadc4c968d1e77
Toni Rachev <tonirachev92@gmail.com>
2025-04-22 13:20:04 +0300

    update

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -8,7 +8,7 @@ export async function POST(req: Request) {
 
     try {
         const { data, error } = await resend.emails.send({
-            from: `Contact Form <onboarding@resend.dev>`,
+            from: `Gritifi <onboarding@resend.dev>`,
             to: ['tonirachev92@gmail.com'],
             subject: `Gritifi new message from ${name} ${surname} <${email}>`,
             html: `<p>${message}</p>`,
