5b35e592330857c9e305610e352d37275a078d16
Toni Rachev <tonirachev92@gmail.com>
2025-04-23 13:29:04 +0300

    update

--- components/button.tsx
+++ components/button.tsx
@@ -9,7 +9,7 @@ type ButtonProps = {
 const buttonStyles = cva('rounded-2xl cursor-pointer font-bold w-[8em]', {
     variants: {
         variant: {
-            primary: 'bg-[#FF6B6B] hover:bg-[#FF6B6B]/80 border-5 border-stone-100 text-stone-100 font-bold min-h-[2.1em] text-[2.6em] lg:text-[2.6em]',
+            primary: 'bg-[#FF6B6B] hover:bg-[#FF6B6B]/80 border-5 border-stone-100 text-stone-100 font-bold min-h-[2.1em] text-[3em] lg:text-[2.6em]',
             submit: 'bg-[#50E3C2] hover:bg-[#50E3C2]/80  text-stone-100 min-h-[2em] text-[3em] lg:text-[1.5em] 2xl:text-[2em]',
         },
     }
